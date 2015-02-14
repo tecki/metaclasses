5142092a86ccb5e9c235dc81cf1329f2c82d2869
Martin Teichmann <lkb.teichmann@gmail.com>
2015-02-14 13:44:07 +0100

    only initialize subclasses
    It is not really possible with current python to do the
    initialization of the class itself. This only initializes
    subclasses.
    
    https://mail.python.org/pipermail/python-ideas/2015-February/031967.html

--- pep422.py
+++ pep422.py
@@ -1,30 +1,24 @@
 class Meta(type):
     @classmethod
-    def __prepare__(cls, name, bases, namespace=None, **kwds):
+    def __prepare__(cls, name, bases, namespace=None, **kwargs):
         if namespace is not None:
             cls.__namespace__ = namespace
         if hasattr(cls, '__namespace__'):
             return cls.__namespace__()
         else:
-            return super().__prepare__(name, bases, **kwds)
+            return super().__prepare__(name, bases, **kwargs)
 
-    def __new__(cls, name, bases, dict, **kwds):
-        if '__init_class__' in dict:
-            dict['__init_class__'] = classmethod(dict['__init_class__'])
+    def __new__(cls, name, bases, dict, **kwargs):
         return super(Meta, cls).__new__(cls, name, bases, dict)
 
-    def __init__(self, name, bases, dict, **kwds):
-        self.__init_class__(**kwds)
+    def __init__(self, name, bases, dict, namespace=None, **kwargs):
+        super(self, self).__init_subclass__(**kwargs)
 
 
-def __init_class__(cls, **kwds):
-    pass
+class Base(object):
+    @classmethod
+    def __init_subclass__(cls, **kwargs):
+        pass
 
-WithInit = Meta("WithInit", (object,), dict(__init_class__=__init_class__))
+SubclassInit = Meta("SubclassInit", (Base,), {})
 
-# black magic: assure everyone is using the same WithInit
-import sys
-if hasattr(sys, "WithInit"):
-    WithInit = sys.WithInit
-else:
-    sys.WithInit = WithInit
