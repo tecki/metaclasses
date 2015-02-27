b7cd280ef381f9f41433d752c5088eed5b90367e
Martin Teichmann <lkb.teichmann@gmail.com>
2015-02-27 16:05:21 +0100

    add setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="metaclass",
+    version="1.0",
+    description="Useful baseclasses for metaclasses",
+    url="https://github.com/tecki/metaclasses",
+    author="Martin Teichmann",
+    author_email="lkb.teichmann@gmail.com",
+    license="PSF",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=["metaclass"]
+)
