aa17cc27252085dadc78e74d37ae150bf5160324
Martin Teichmann <lkb.teichmann@gmail.com>
2015-02-27 15:22:05 +0100

    make namespaces actually work

--- metaclass.py
+++ metaclass.py
@@ -1,12 +1,10 @@
 class Meta(type):
     @classmethod
     def __prepare__(cls, name, bases, namespace=None, **kwargs):
-        if namespace is not None:
-            cls.__namespace__ = namespace
-        if hasattr(cls, '__namespace__'):
-            return cls.__namespace__()
-        else:
-            return super().__prepare__(name, bases, **kwargs)
+        for b in bases:
+            if isinstance(b, Meta) and hasattr(b, "__namespace__"):
+                return b.__namespace__()
+        return super().__prepare__(name, bases, **kwargs)
 
     def __new__(cls, name, bases, dict, **kwargs):
         method = dict.get("__init_subclass__")
@@ -16,6 +14,8 @@ class Meta(type):
 
     def __init__(self, name, bases, dict, namespace=None, **kwargs):
         super(self, self).__init_subclass__(**kwargs)
+        if namespace is not None:
+            self.__namespace__ = namespace
 
 
 class Base(object):
--- tests.py
+++ tests.py
@@ -153,6 +153,30 @@ class Test(TestCase):
             S = types.new_class("S", (BaseS,))
         self.assertIs(S, sentinel)
 
+    def test_namespace(self):
+        outer = self
+        class NS(dict):
+            def __init__(self):
+                super().__init__(bar=3)
+            def __setitem__(self, attr, value):
+                if not attr.startswith("__"):
+                    NS.called_set = True
+                    outer.assertEqual(attr, "spam")
+                    outer.assertEqual(value, "ham")
+                super().__setitem__(attr, value)
+            def __getitem__(self, attr):
+                if not attr.startswith("__"):
+                    NS.called_get = True
+                    outer.assertIn(attr, ("outer", "bar"))
+                return super().__getitem__(attr)
+        class Base(SubclassInit, namespace=NS):
+            pass
+        class Class(Base):
+            outer.assertEqual(bar, 3)
+            spam = "ham"
+        self.assertTrue(NS.called_set)
+        self.assertTrue(NS.called_get)
+
 
 
 if __name__ == "__main__":
